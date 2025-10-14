863d952f669320cfb4e05635456190d11d7c1985
fuwenjiang <fuwenjiang@fuwenjiangdeMacBook-Pro.local>
2025-10-14 18:06:19 +0800

    创建项目

---
+++ packages/trading-view-replay/src/index.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param a The first number
+ * @param b The second number
+ * @returns The sum of a and b
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+/**
+ *
+ * @param a The first number
+ * @param b The second number
+ * @returns The sum of a and b
+ */
+export function reduce(a: number, b: number) {
+  return a- b
+}
